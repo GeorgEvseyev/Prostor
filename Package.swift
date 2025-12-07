27dda96406ac69776319a368bad534df36f49ac0
GeorgEvseyev <evseyevgeorg@gmail.com>
2025-12-07 12:59:03 +0300

    chore(spm): use forked swift-command-line-tools v0.1.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.6.2")),
-        .package(url: "https://github.com/element-hq/swift-command-line-tools.git", revision: "e5eaab1558ef664e6cd80493f64259381670fb3a"),
-        // .package(path: "../../../swift-command-line-tools"),
+        .package(url: "https://github.com/GeorgEvseyev/swift-command-line-tools.git", from: "0.1.0"),
+
         .package(url: "https://github.com/jpsim/Yams", .upToNextMinor(from: "6.2.0"))
     ],
     targets: [
